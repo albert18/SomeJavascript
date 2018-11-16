70c59864ca7207c6992ed06f2e2203960678074e
albert18 <caps18@hotmail.com>
2018-11-16 07:59:22 -0500

    some function

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
 // This is some test!!
 alert('test');
+
+
+// function makeCoffee(sugar, percent)
+// {
+//     var instructions = "Boil Water";
+
+//     instructions += "pour into cup";
+
+//     instructions += " add " + sugar + "spoon of sugar";
+
+//     instructions += " and some" + percent; 
+
+//     return instructions;
+// };
+
+// console.log(makeCoffee(5,10));
+// --------------------------------------------------------------------------------------
